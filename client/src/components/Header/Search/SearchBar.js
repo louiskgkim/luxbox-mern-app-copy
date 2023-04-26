45911557f167b6f8433903e33394bc9539bb05f7
Claire <claire.hw.cho@gmail.com>
2023-04-26 15:29:35 -0700

    remove sliding effect of search bar when input is clicked

--- client/src/components/Header/Search/SearchBar.js
+++ client/src/components/Header/Search/SearchBar.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 import SearchIcon from '@mui/icons-material/SearchOutlined';
@@ -9,18 +9,9 @@ const SearchBar = (props) => {
 
     const navigate = useNavigate();
 
-    const openSearchBar = () => {
-        document.getElementById("hidden-search-bar").style.height = "8.7rem";
-    };
-
-    const closeSearchBar = () => {
-        document.getElementById("hidden-search-bar").style.height = "0";
-    };
-
     const handleSearchChange = (e) => {
         setSearchInput(e.target.value);
     }
-
 
     const handleSearchSubmit = (e) => {
         e.preventDefault();
@@ -29,83 +20,42 @@ const SearchBar = (props) => {
 
     return (
         <div className="search-bar-wrapper">
-            <div className="hidden-search-bar" id="hidden-search-bar">
-                <div className="hidden-search-bar-row">
-                    <h1>LuxBox</h1>
-                    <Link
-                        to={undefined}
-                        className="search-bar-close-btn link"
-                        onClick={closeSearchBar}
-                    >&times;</Link>
-                </div>
-                <div className="hidden-search-bar-row">
-                    <form
-                        className="search-form"
-                        onSubmit={handleSearchSubmit}
-                    >
-                        <TextField
-                            className="hidden-search-bar-input"
-                            id="input-with-icon-textfield"
-                            placeholder="Search"
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <SearchIcon
-                                            sx={{
-                                                color: "white",
-                                            }}
-                                        />
-                                    </InputAdornment>
-                                ),
-                                autoFocus: true
-                            }}
-                            variant="standard"
-                            focused
-                            sx={{
-                                input: {
-                                    color: "white",
-                                    borderBottom: "1px solid white",
-                                },
-                                "& .MuiInput-underline:after": {
-                                    borderBottom: "1px solid white",
-                                },
-                            }}
-                            onChange={handleSearchChange}
-                        />
-                    </form>
-                </div>
-            </div>
-            <div className="visible-search-bar">
-                <TextField
-                    className="visible-search-bar-input"
-                    id="input-with-icon-textfield"
-                    placeholder="Search"
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <SearchIcon
-                                    sx={{
-                                        color: "white",
-                                    }}
-                                />
-                            </InputAdornment>
-                        ),
-                    }}
-                    variant="standard"
-                    focused
-                    sx={{
-                        input: {
-                            fontSize: "0.8rem",
-                            color: "white",
-                            borderBottom: "1px solid white",
-                        },
-                        "& .MuiInput-underline:after": {
-                            borderBottom: "1px solid white",
-                        },
-                    }}
-                    onClick={openSearchBar}
-                />
-            </div>
+            <div className="search-bar">
+                <form
+                    className="search-form"
+                    onSubmit={handleSearchSubmit}
+                >
+                    <TextField
+                        className="search-bar-input"
+                        id="input-with-icon-textfield"
+                        placeholder="Search"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <SearchIcon
+                                        sx={{
+                                            color: "white",
+                                        }}
+                                    />
+                                </InputAdornment>
+                            ),
+                        }}
+                        variant="standard"
+                        focused
+                        sx={{
+                            input: {
+                                fontSize: "0.8rem",
+                                color: "white",
+                                borderBottom: "1px solid white",
+                            },
+                            "& .MuiInput-underline:after": {
+                                borderBottom: "1px solid white",
+                            },
+                        }}
+                        onChange={handleSearchChange}
+                    />
+                </form>
+            </div >
         </div >
     );
 };
