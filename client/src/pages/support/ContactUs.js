40388d77b9d4e0bfa939cd8e69713cae644b0662
Claire <claire.hw.cho@gmail.com>
2023-04-30 19:39:01 -0700

    complete contact us page

--- client/src/pages/support/ContactUs.js
+++ client/src/pages/support/ContactUs.js
@@ -1,5 +1,9 @@
 import { Link } from 'react-router-dom';
 
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+
 import EmailForm from '../../components/Forms/EmailForm';
 
 const ContactUs = (props) => {
@@ -9,8 +13,85 @@ const ContactUs = (props) => {
                 <h3>Contact Us</h3>
             </div>
             <div className="main-content-row">
-                <EmailForm />
-                {/* Add our team member's github usernames and make links to the accounts */}
+                <div className="contact-us-wrapper">
+                    <div className="contact-info-wrapper">
+                        <p>For any enquiries please contact us. We're available 24 hours, 7 days a week.</p>
+                        <div className="developer-info">
+                            <p>Claire Hyung Won Cho</p>
+                            <div className="developer-link">
+                                <Link to="https://github.com/clairehwcho" target="_blank" className="link">
+                                    <GitHubIcon />
+                                </Link>
+                                <Link to="https://www.linkedin.com/in/hyungwoncho/" target="_blank" className="link">
+                                    <LinkedInIcon />
+                                </Link>
+                                <Link to="mailto:claire.hw.cho@gmail.com" className="link">
+                                    <EmailIcon />
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="developer-info">
+                            <p>Edgar Vargas</p>
+                            <div className="developer-link">
+                                <Link to="https://github.com/e-varg24" target="_blank" className="link">
+                                    <GitHubIcon />
+                                </Link>
+                                <Link to="" target="_blank" className="link">
+                                    <LinkedInIcon />
+                                </Link>
+                                <Link to="" className="link">
+                                    <EmailIcon />
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="developer-info">
+                            <p>Faye Hong</p>
+                            <div className="developer-link">
+                                <Link to="https://github.com/hong-f" target="_blank" className="link">
+                                    <GitHubIcon />
+                                </Link>
+                                <Link to="" target="_blank" className="link">
+                                    <LinkedInIcon />
+                                </Link>
+                                <Link to="" className="link">
+                                    <EmailIcon />
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="developer-info">
+                            <p>Louis Kim</p>
+                            <div className="developer-link">
+                                <Link to="https://github.com/louiskgkim" target="_blank" className="link">
+                                    <GitHubIcon />
+                                </Link>
+                                <Link to="" target="_blank" className="link">
+                                    <LinkedInIcon />
+                                </Link>
+                                <Link to="" className="link">
+                                    <EmailIcon />
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="developer-info">
+                            <p>Jordon Lor</p>
+                            <div className="developer-link">
+                                <Link to="https://github.com/JordonLo" target="_blank" className="link">
+                                    <GitHubIcon />
+                                </Link>
+                                <Link to="" target="_blank" className="link">
+                                    <LinkedInIcon />
+                                </Link>
+                                <Link to="" className="link">
+                                    <EmailIcon />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="contact-us-email-form-wrapper">
+                        <p>Or email us now.</p>
+                        <EmailForm />
+                    </div>
+                </div>
             </div>
         </section>
     );
