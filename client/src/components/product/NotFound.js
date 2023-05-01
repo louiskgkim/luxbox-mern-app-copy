4177554ff9c587734cf14570e318ff4999dae83c
Claire <claire.hw.cho@gmail.com>
2023-05-01 11:49:56 -0700

    test commit

--- client/src/components/product/NotFound.js
+++
@@ -1,9 +0,0 @@
-const NotFound = (props) => {
-    return (
-        <section className="not-found-wrapper">
-            <p>Items not found.</p>
-        </section >
-    );
-};
-
-export default NotFound;
