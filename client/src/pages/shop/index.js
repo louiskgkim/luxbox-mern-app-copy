fdea51b6f9db06162d274cf11643455a60ede5fb
louiskgkim <lkgk31@gmail.com>
2023-04-30 23:09:24 -0700

    removing removed files

--- client/src/pages/shop/index.js
+++
@@ -1,5 +0,0 @@
-import DesignerList from './DesignerList';
-import ProductList from './ProductList';
-import ProductDetail from './ProductDetail';
-
-export { DesignerList, ProductList, ProductDetail };
